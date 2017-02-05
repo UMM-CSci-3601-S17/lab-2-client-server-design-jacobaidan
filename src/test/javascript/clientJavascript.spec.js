472c708e81ad7920eeb47fa66cc335a5726a9931
sphat001 <sphat001@morris.umn.edu>
2017-02-05 13:36:47 -0600

    Created some new tests on the client and server sides. Had issues with Java testing

--- src/test/javascript/clientJavascript.spec.js
+++ src/test/javascript/clientJavascript.spec.js
@@ -1,17 +1,27 @@
 // Just some example testing.
 
 describe('testing basic functions', function(){
-    it('should return the correct string length', function(){
-        expect(testStringLength("kittens")).toEqual(7);
+    // it('should return the correct string length', function(){
+    //     expect(testStringLength("kittens")).toEqual(7);
+    // });
+    it('should return the correct url', function() {
+        expect(buildURL("","","","")).toEqual("/api/todos?" + "limit="  + "&status=" + "&contains=");
     });
 
-    it('returnKittens should return kittens', function(){
-        expect(returnKittens()).toBe("kittens");
+    it('should return the correct url', function() {
+        expect(buildURL("58895985ae3b752b124e7663","","complete","")).toEqual("/api/todos/" + "58895985ae3b752b124e7663");
     });
 
-    ////this test should fail.
-    it('this test should fail!', function(){
-        expect(returnKittens()).toBe("Not Kittens!");
+    it('should return the correct url', function() {
+        expect(buildURL("","10","incomplete","")).toEqual("/api/todos?" + "limit="  + 10 + "&status=" + "incomplete" + "&contains=");
     });
+
+    it('should return the correct url', function() {
+        expect(buildURL("","","complete","banana")).toEqual("/api/todos?" + "limit="  + "&status=" + "complete" + "&contains=" + "banana");
+    });
+
+    it('should return the correct url', function() {
+        expect(buildURL("","11","complete","banana")).toEqual("/api/todos?" + "limit=" + 11  + "&status=" + "complete" + "&contains=" + "banana");
+    });
+
 });
-
